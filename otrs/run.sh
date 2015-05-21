8f31fdfb7d828a9fdf9f5300ca371011a2b53ef2
Juan Luis Baptiste <juan.baptiste@gmail.com>
2015-05-21 18:22:16 -0500

    Moved checks inside load_defaults function.

--- otrs/run.sh
+++ otrs/run.sh
@@ -23,15 +23,7 @@ DEFAULT_OTRS_ADMIN_EMAIL="admin@example.com"
 DEFAULT_OTRS_ORGANIZATION="Example Company"
 DEFAULT_OTRS_SYSTEM_ID="98"
 
-[ -z "${LOAD_BACKUP}" ] && LOAD_BACKUP="no"
 [ -z "${OTRS_INSTALL}" ] && OTRS_INSTALL="no"
-
-[ -z "${OTRS_HOSTNAME}" ] && OTRS_HOSTNAME="otrs-`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1`" && echo "OTRS_ROOT_HOSTNAME not set, setting hostname to '$OTRS_HOSTNAME'"
-[ -z "${OTRS_ADMIN_EMAIL}" ] && echo "OTRS_ADMIN_EMAIL not set, setting admin email to '$DEFAULT_OTRS_ADMIN_EMAIL'" && OTRS_ADMIN_EMAIL=$DEFAULT_OTRS_ADMIN_EMAIL
-[ -z "${OTRS_ORGANIZATION}" ] && echo "OTRS_ORGANIZATION setting organization to '$DEFAULT_OTRS_ORGANIZATION'" && OTRS_ORGANIZATION=$DEFAULT_OTRS_ORGANIZATION
-[ -z "${OTRS_SYSTEM_ID}" ] && echo "OTRS_SYSTEM_ID not set, setting System ID to '$DEFAULT_OTRS_SYSTEM_ID'"  && OTRS_SYSTEM_ID=$DEFAULT_OTRS_SYSTEM_ID
-[ -z "${OTRS_DB_PASSWORD}" ] && echo "OTRS_DB_PASSWORD not set, setting password to '$DEFAULT_OTRS_PASSWORD'" && OTRS_DB_PASSWORD=$DEFAULT_OTRS_PASSWORD
-[ -z "${OTRS_ROOT_PASSWORD}" ] && echo "OTRS_ROOT_PASSWORD not set, setting password to '$DEFAULT_OTRS_PASSWORD'" && OTRS_ROOT_PASSWORD=$DEFAULT_OTRS_PASSWORD
 
 mysqlcmd="mysql -uroot -h $MARIADB_PORT_3306_TCP_ADDR -p$MARIADB_ENV_MYSQL_ROOT_PASSWORD "
 
@@ -48,6 +40,13 @@ function restore_backup(){
 }
 
 function load_defaults(){
+  [ -z "${OTRS_HOSTNAME}" ] && OTRS_HOSTNAME="otrs-`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1`" && echo "OTRS_ROOT_HOSTNAME not set, setting hostname to '$OTRS_HOSTNAME'"
+  [ -z "${OTRS_ADMIN_EMAIL}" ] && echo "OTRS_ADMIN_EMAIL not set, setting admin email to '$DEFAULT_OTRS_ADMIN_EMAIL'" && OTRS_ADMIN_EMAIL=$DEFAULT_OTRS_ADMIN_EMAIL
+  [ -z "${OTRS_ORGANIZATION}" ] && echo "OTRS_ORGANIZATION setting organization to '$DEFAULT_OTRS_ORGANIZATION'" && OTRS_ORGANIZATION=$DEFAULT_OTRS_ORGANIZATION
+  [ -z "${OTRS_SYSTEM_ID}" ] && echo "OTRS_SYSTEM_ID not set, setting System ID to '$DEFAULT_OTRS_SYSTEM_ID'"  && OTRS_SYSTEM_ID=$DEFAULT_OTRS_SYSTEM_ID
+  [ -z "${OTRS_DB_PASSWORD}" ] && echo "OTRS_DB_PASSWORD not set, setting password to '$DEFAULT_OTRS_PASSWORD'" && OTRS_DB_PASSWORD=$DEFAULT_OTRS_PASSWORD
+  [ -z "${OTRS_ROOT_PASSWORD}" ] && echo "OTRS_ROOT_PASSWORD not set, setting password to '$DEFAULT_OTRS_PASSWORD'" && OTRS_ROOT_PASSWORD=$DEFAULT_OTRS_PASSWORD
+
   create_db
   echo -e "Loading default db schema..."  
   $mysqlcmd otrs < /opt/otrs/scripts/database/otrs-schema.mysql.sql
