fe00584b529da2d94790e9489e2a88d71d16a730
Juan Luis Baptiste <juan.baptiste@gmail.com>
2015-05-22 04:00:06 -0500

    Move random string generation to its own function and use random password if OTRS_DB_PASSWORD isn't defined.

--- otrs/run.sh
+++ otrs/run.sh
@@ -17,7 +17,6 @@
 # - OTRS_DB_PASSWORD to set the database password
 # - OTRS_ROOT_PASSWORD to set the admin user 'root@localhost' password. 
 #
-DEFAULT_OTRS_PASSWORD="changeme"
 env
 
 DEFAULT_OTRS_ADMIN_EMAIL="admin@example.com"
@@ -40,12 +39,16 @@ function restore_backup(){
     [ $? -gt 0 ] && echo -e "\n\e[1;31mERROR:\e[0m Couldn't load OTRS backup !!\n" && exit 1
 }
 
+function random_string(){
+  echo `cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1`
+}
+
 function load_defaults(){
-  [ -z "${OTRS_HOSTNAME}" ] && OTRS_HOSTNAME="otrs-`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1`" && echo "OTRS_ROOT_HOSTNAME not set, setting hostname to '$OTRS_HOSTNAME'"
+  [ -z "${OTRS_HOSTNAME}" ] && OTRS_HOSTNAME="otrs-`random_string`" && echo "OTRS_ROOT_HOSTNAME not set, setting hostname to '$OTRS_HOSTNAME'"
   [ -z "${OTRS_ADMIN_EMAIL}" ] && echo "OTRS_ADMIN_EMAIL not set, setting admin email to '$DEFAULT_OTRS_ADMIN_EMAIL'" && OTRS_ADMIN_EMAIL=$DEFAULT_OTRS_ADMIN_EMAIL
   [ -z "${OTRS_ORGANIZATION}" ] && echo "OTRS_ORGANIZATION setting organization to '$DEFAULT_OTRS_ORGANIZATION'" && OTRS_ORGANIZATION=$DEFAULT_OTRS_ORGANIZATION
   [ -z "${OTRS_SYSTEM_ID}" ] && echo "OTRS_SYSTEM_ID not set, setting System ID to '$DEFAULT_OTRS_SYSTEM_ID'"  && OTRS_SYSTEM_ID=$DEFAULT_OTRS_SYSTEM_ID
-  [ -z "${OTRS_DB_PASSWORD}" ] && echo "OTRS_DB_PASSWORD not set, setting password to '$DEFAULT_OTRS_PASSWORD'" && OTRS_DB_PASSWORD=$DEFAULT_OTRS_PASSWORD
+  [ -z "${OTRS_DB_PASSWORD}" ] && OTRS_DB_PASSWORD=`random_string` && echo "OTRS_DB_PASSWORD not set, setting password to '$OTRS_DB_PASSWORD'"
   [ -z "${OTRS_ROOT_PASSWORD}" ] && echo "OTRS_ROOT_PASSWORD not set, setting password to '$DEFAULT_OTRS_PASSWORD'" && OTRS_ROOT_PASSWORD=$DEFAULT_OTRS_PASSWORD
 
   create_db
