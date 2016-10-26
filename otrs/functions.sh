2857a794105c21598448bcd84b811ecf2aa47c0f
Juan Luis Baptiste <juan.baptiste@gmail.com>
2016-10-26 01:57:22 -0500

    Removed for now as it breaks the configuration file.

--- otrs/functions.sh
+++ otrs/functions.sh
@@ -163,11 +163,7 @@ function load_defaults(){
 \n\$Self->{'Organization'} = '$OTRS_ORGANIZATION';\
 \n\$Self->{'CustomerHeadline'} = '$OTRS_ORGANIZATION';\
 \n\$Self->{'SystemID'} = '$OTRS_SYSTEM_ID';\
-\n\$Self->{'PostMaster::PreFilterModule::NewTicketReject::Sender'} = 'noreply@${OTRS_HOSTNAME}';\
-\n\$Self->{'PostmasterFollowUpSearchInRaw'} = 1;\
-\n\$Self->{'PostmasterFollowUpSearchInBody'} = 1;\
-\n\$Self->{'PostmasterFollowUpSearchInAttachment'} = 1;\
-\n\$Self->{'PostmasterFollowUpSearchInReferences'} = 1;"\
+\n\$Self->{'PostMaster::PreFilterModule::NewTicketReject::Sender'} = 'noreply@${OTRS_HOSTNAME}';"\
  ${OTRS_ROOT}Kernel/Config.pm
 
   #Check if database doesn't exists yet (it could if this is a container redeploy)
--- otrs/run.sh
+++ otrs/run.sh
@@ -53,7 +53,7 @@ if [ "$OTRS_INSTALL" != "yes" ]; then
   set_default_language
   rm -fr ${OTRS_ROOT}var/tmp/firsttime
   #Start OTRS
-  ${OTRS_ROOT}bin/otrs.SetPermissions.pl --otrs-user=otrs --web-group=apache /opt/otrs
+  ${OTRS_ROOT}bin/otrs.SetPermissions.pl --otrs-user=otrs --web-group=apache ${OTRS_ROOT}
   ${OTRS_ROOT}bin/Cron.sh start otrs
   /usr/bin/perl ${OTRS_ROOT}bin/otrs.Scheduler.pl -w 1
   set_fetch_email_time  
