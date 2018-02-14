1cb5f3f6ff8baeae053ffa8b5dd87cc59d5f995a
Juan Luis Baptiste <juan.baptiste@gmail.com>
2018-02-14 15:59:24 -0500

    Use MYSQL_ROOT_PASSWORD env variable instead of the old *_env_* as they arent generated on rancher.

--- otrs/functions.sh
+++ otrs/functions.sh
@@ -37,7 +37,7 @@ OTRS_CONFIG_MOUNT_DIR="/Kernel"
 
 [ -z "${OTRS_INSTALL}" ] && OTRS_INSTALL="no"
 
-mysqlcmd="mysql -uroot -h mariadb -p$MARIADB_ENV_MYSQL_ROOT_PASSWORD "
+mysqlcmd="mysql -uroot -h mariadb -p${MYSQL_ROOT_PASSWORD} "
 
 function create_db(){
   print_info "Creating OTRS database..."
