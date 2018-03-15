060b81993c8751a4cb384117c15cabc6f9235f94
Juan Luis Baptiste <juan.baptiste@gmail.com>
2018-03-15 16:50:33 -0500

    Make otrs_backup.sh compress the final backup directory into a single .gz file

--- otrs/otrs_backup.sh
+++ otrs/otrs_backup.sh
@@ -8,29 +8,38 @@ OTRS_BACKUP_DIR="/var/otrs/backups"
 DEFAULT_BACKUP_TYPE="fullbackup"
 
 function get_current_date(){
-   date "+[%d-%m-%Y_%H:%M]"
+   date "+%d-%m-%Y_%H_%M"
 }
+
+DATE=$(get_current_date)
+BACKUP_FILE_NAME="otrs-${DATE}-full.tar.bz2"
 
 BACKUP_TYPE=$1
 [ -z $BACKUP_TYPE ] && BACKUP_TYPE=$DEFAULT_BACKUP_TYPE
 
-echo -e "`get_current_date` Starting OTRS backup for host ${OTRS_HOSTNAME}..."
+echo -e "[${DATE}] Starting OTRS backup for host ${OTRS_HOSTNAME}..."
 [ ! -e $TEMP_BACKUP_DIR ] && mkdir -p $TEMP_BACKUP_DIR
 
 /opt/otrs/scripts/backup.pl -d $TEMP_BACKUP_DIR -t $BACKUP_TYPE
 
 if [ $? -eq 0 ]; then
   [ ! -e $OTRS_BACKUP_DIR ] && mkdir -p $OTRS_BACKUP_DIR
-  mv $TEMP_BACKUP_DIR/* $OTRS_BACKUP_DIR
+  #cd ${TEMP_BACKUP_DIR}
+  # As the otrs backup command throws three separate backups in a directory, we
+  # compress those files into a single one
+  tar zcvf ${BACKUP_FILE_NAME} ${TEMP_BACKUP_DIR}/*
+  [ $? -gt 0 ] && echo -e "ERROR: Could not compress final backup tarball." && exit 1
+
+  mv ${BACKUP_FILE_NAME} $OTRS_BACKUP_DIR
   if [ $? -gt 0 ]; then
     echo -e "Backup files move to $OTRS_BACKUP_DIR failed."
     exit 1
   else
     chmod -R 755 $OTRS_BACKUP_DIR
-    echo -e "`get_current_date` Backup successful."
+    echo -e "${DATE} Backup successful."
   fi
 else
-  echo -e "Backup failed."
+  echo -e "ERROR: Backup process failed."
   exit 1
 fi
 
