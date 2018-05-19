79c57d7aaafd38ec7397f167e2400192192cb500
Juan Luis Baptiste <juan.baptiste@gmail.com>
2018-05-18 20:14:16 -0500

    Only reinstall modules if nodoing an uprade.

--- otrs/functions.sh
+++ otrs/functions.sh
@@ -323,13 +323,15 @@ function enable_debug_mode () {
 }
 
 function reinstall_modules () {
-  print_info "Reinstalling OTRS modules..."
-  su -c "${OTRS_ROOT}/bin/otrs.Console.pl Admin::Package::ReinstallAll > /dev/null 2>&1" -s /bin/bash otrs
+  if [ "${OTRS_UPGRADE}" != "yes" ]; then
+    print_info "Reinstalling OTRS modules..."
+    su -c "$OTRS_ROOT/bin/otrs.Console.pl Admin::Package::ReinstallAll > /dev/null 2>&1" -s /bin/bash otrs
 
-  if [ $? -gt 0 ]; then
-    print_error "Could not reinstall OTRS modules, try to do it manually with the Package Manager at the admin section."
-  else
-    print_info "Done."
+    if [ $? -gt 0 ]; then
+      print_error "Could not reinstall OTRS modules, try to do it manually with the Package Manager at the admin section."
+    else
+      print_info "Done."
+    fi
   fi
 }
 
