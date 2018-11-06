c6ff6b388de02d44f5ec829a3eacca41497b9603
Juan Luis Baptiste <juan.baptiste@gmail.com>
2018-11-05 21:47:52 -0500

    Also set OTRS_DB_NAME on setup_otrs_config (Issue #45)

--- otrs/functions.sh
+++ otrs/functions.sh
@@ -89,6 +89,7 @@ function restore_backup() {
   add_config_value "DatabasePw" ${OTRS_DB_PASSWORD}
   add_config_value "DatabaseHost" ${OTRS_DB_HOST}
   add_config_value "DatabasePort" ${OTRS_DB_PORT}
+  add_config_value "Database" ${OTRS_DB_NAME}
 
   #Check first that the backup file exists
   restore_file="${OTRS_BACKUP_DIR}/${OTRS_BACKUP_DATE}"
@@ -188,6 +189,7 @@ function setup_otrs_config() {
   add_config_value "DatabaseUser" ${OTRS_DB_USER}
   add_config_value "DatabasePw" ${OTRS_DB_PASSWORD}
   add_config_value "DatabaseHost" ${OTRS_DB_HOST}
+  add_config_value "Database" ${OTRS_DB_NAME}
   #Set general configuration values
   [ ! -z "${OTRS_LANGUAGE}" ] && add_config_value "DefaultLanguage" ${OTRS_LANGUAGE}
   [ ! -z "${OTRS_TIMEZONE}" ] && add_config_value "OTRSTimeZone" ${OTRS_TIMEZONE} && add_config_value "UserDefaultTimeZone" ${OTRS_TIMEZONE}
